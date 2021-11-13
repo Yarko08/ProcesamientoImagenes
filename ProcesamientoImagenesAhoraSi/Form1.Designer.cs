c06019e4f4e157b944b3f3f56693962a23f26fc2
Yarko08 <72364834+Yarko08@users.noreply.github.com>
2021-11-13 12:01:54 -0600

    Avanze 1
    ASDA

---
+++ ProcesamientoImagenesAhoraSi/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace ProcesamientoImagenesAhoraSi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Deshacer = new System.Windows.Forms.Button();
+            this.Guardar = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.Buscar = new System.Windows.Forms.Button();
+            this.Camara = new System.Windows.Forms.Button();
+            this.Tomar = new System.Windows.Forms.Button();
+            this.Aplicar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(240, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(227, 223);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(536, 56);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(227, 223);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Deshacer
+            // 
+            this.Deshacer.Location = new System.Drawing.Point(536, 299);
+            this.Deshacer.Name = "Deshacer";
+            this.Deshacer.Size = new System.Drawing.Size(75, 23);
+            this.Deshacer.TabIndex = 2;
+            this.Deshacer.Text = "Deshacer";
+            this.Deshacer.UseVisualStyleBackColor = true;
+            // 
+            // Guardar
+            // 
+            this.Guardar.Location = new System.Drawing.Point(617, 299);
+            this.Guardar.Name = "Guardar";
+            this.Guardar.Size = new System.Drawing.Size(75, 23);
+            this.Guardar.TabIndex = 3;
+            this.Guardar.Text = "Guardar";
+            this.Guardar.UseVisualStyleBackColor = true;
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(698, 299);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(75, 23);
+            this.Salir.TabIndex = 4;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            // 
+            // Buscar
+            // 
+            this.Buscar.Location = new System.Drawing.Point(406, 299);
+            this.Buscar.Name = "Buscar";
+            this.Buscar.Size = new System.Drawing.Size(61, 23);
+            this.Buscar.TabIndex = 5;
+            this.Buscar.Text = "Buscar...";
+            this.Buscar.UseVisualStyleBackColor = true;
+            // 
+            // Camara
+            // 
+            this.Camara.Location = new System.Drawing.Point(380, 22);
+            this.Camara.Name = "Camara";
+            this.Camara.Size = new System.Drawing.Size(65, 23);
+            this.Camara.TabIndex = 6;
+            this.Camara.Text = "On/Off Camare";
+            this.Camara.UseVisualStyleBackColor = true;
+            this.Camara.Click += new System.EventHandler(this.Camara_Click);
+            // 
+            // Tomar
+            // 
+            this.Tomar.Location = new System.Drawing.Point(262, 299);
+            this.Tomar.Name = "Tomar";
+            this.Tomar.Size = new System.Drawing.Size(75, 23);
+            this.Tomar.TabIndex = 7;
+            this.Tomar.Text = "Tomar";
+            this.Tomar.UseVisualStyleBackColor = true;
+            // 
+            // Aplicar
+            // 
+            this.Aplicar.Location = new System.Drawing.Point(43, 299);
+            this.Aplicar.Name = "Aplicar";
+            this.Aplicar.Size = new System.Drawing.Size(65, 23);
+            this.Aplicar.TabIndex = 8;
+            this.Aplicar.Text = "Aplicar";
+            this.Aplicar.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(43, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 212);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Buscar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(240, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 334);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Aplicar);
+            this.Controls.Add(this.Tomar);
+            this.Controls.Add(this.Camara);
+            this.Controls.Add(this.Buscar);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Guardar);
+            this.Controls.Add(this.Deshacer);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Deshacer;
+        private System.Windows.Forms.Button Guardar;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button Buscar;
+        private System.Windows.Forms.Button Camara;
+        private System.Windows.Forms.Button Tomar;
+        private System.Windows.Forms.Button Aplicar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ProcesamientoImagenesAhoraSi/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
+
+namespace ProcesamientoImagenesAhoraSi
+{
+    public partial class Form1 : Form
+    {
+        private bool Haydispositivo;
+        private FilterInfoCollection Misdispotiivos;
+        private VideoCaptureDevice miwebcam;
+        private bool onof = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDispositivos();
+        }
+
+        public void CargarDispositivos()
+        {
+            Misdispotiivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (Misdispotiivos.Count > 0)
+            {
+                Haydispositivo = true;
+                for (int i = 0; i < Misdispotiivos.Count; i++)
+                    comboBox1.Items.Add(Misdispotiivos[i].Name.ToString());
+                comboBox1.Text = Misdispotiivos[0].ToString();
+            }
+            else
+                Haydispositivo = false;
+        }
+
+        private void Camara_Click(object sender, EventArgs e)
+        {
+            if (onof ==false) { 
+            webcam();
+            int i = comboBox1.SelectedIndex;
+            string NombreVideo = Misdispotiivos[i].MonikerString;
+            miwebcam = new VideoCaptureDevice(NombreVideo);
+            miwebcam.NewFrame += new NewFrameEventHandler(Capturando);
+            miwebcam.Start();
+                onof = true;
+                 }
+            else
+            {
+                webcam();
+                onof = false;
+
+            }
+        }
+
+        private void Capturando(object sender,NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = Imagen;
+        }
+        private void webcam()
+        {
+            if(miwebcam!=null && miwebcam.IsRunning)
+            {
+                miwebcam.SignalToStop();
+                miwebcam = null;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            webcam();
+        }
+    }
+}
